e6c142a57e0a820da304b6e42742aa54b5ab5efb
korebhaumik <bhaumik.kore31@gmail.com>
2022-12-23 13:48:02 +0530

    cart update

--- src/presentation/components/filledCart.js
+++ src/presentation/components/filledCart.js
@@ -1,5 +1,85 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { decre } from "../../logic/actions/cartActions";
 
 export default function FilledCart() {
-  return <div>FilledCart</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const cartData = useSelector((state) => state.cartData);
+  const newArr = cartData.map((item) => {
+    return (
+      <div style={{ marginTop: "1em" }} key={Math.random()}>
+        <div className="filled-unit">
+          <div>
+            <img
+              className="filled-img"
+              src={
+                "https://storage.googleapis.com/bhaumikbucket/id13_post.jpeg"
+              }
+            />
+            <h1 className="filled-book-title">{item.name}</h1>
+          </div>
+          <h2 className="filled-book-quantity">x{item.quantity}</h2>
+          <h2 className="filled-book-price">Rs. {500}</h2>
+          <h2 className="filled-book-price">Rs. {item.quantity * 500}</h2>
+          {/* <svg
+            onClick={() => dispatch(decre({ ...item, quantity: 1 }))}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="cross"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg> */}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="filled-container">
+      <div className="filled-left">
+        <div className="filled-heading">
+          <h1>Shopping Cart</h1>
+          <h2>3 items</h2>
+        </div>
+        <hr />
+
+        <div className="filled-titles">
+          <h3 className="details">PRODUCT DETAILS</h3>
+          <h3>QUANTITY</h3>
+          <h3>PRICE</h3>
+          <h3>TOTAL</h3>
+        </div>
+        <div className="filled-scrollable">{newArr}</div>
+      </div>
+      <div className="filled-right">
+        <h1>Summary</h1>
+        <div>
+          <h2>Total:</h2>
+          <h2>Rs. 1500</h2>
+        </div>
+        <div>
+          <h2>Discount:</h2>
+          <h2>10%</h2>
+        </div>
+        <div>
+          <h2>Shipping:</h2>
+          <h2>Rs. 200</h2>
+        </div>
+        <div className="subtotal">
+          <h2>Subtotal:</h2>
+          <h2>Rs. 200</h2>
+        </div>
+        <button>Checkout Items</button>
+      </div>
+    </div>
+  );
 }
--- src/presentation/pages/cart.js
+++ src/presentation/pages/cart.js
@@ -40,46 +40,48 @@ export default function Cart() {
     );
   });
   return (
-    // <div className="cart-container"><FilledCart /></div>
-    <div className="cart-container">
+    // <FilledCart />
+    <>
       {cartData.length ? (
         <FilledCart />
       ) : (
-        <div className="empty-combine">
-          <div className="empty-container">
-            <h1>
-              Oops Your <span>cart</span> is empty!!
-            </h1>
-            <p>Looks like you haven’t added anything to your cart.</p>
-            <div
-              className="empty-link"
-              onClick={() => {
-                navigate("/books");
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                className="arrow"
+        <div className="cart-container">
+          <div className="empty-combine">
+            <div className="empty-container">
+              <h1>
+                Oops Your <span>cart</span> is empty!!
+              </h1>
+              <p>Looks like you haven’t added anything to your cart.</p>
+              <div
+                className="empty-link"
+                onClick={() => {
+                  navigate("/books");
+                }}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-                />
-              </svg>
-              <p>Continue Shopping</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  className="arrow"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                  />
+                </svg>
+                <p>Continue Shopping</p>
+              </div>
             </div>
+            <img
+              className="empty-illu"
+              src="https://cdn.discordapp.com/attachments/1053692777470378016/1055491646785077278/Group_10.png"
+            />
           </div>
-          <img
-            className="empty-illu"
-            src="https://cdn.discordapp.com/attachments/1053692777470378016/1055491646785077278/Group_10.png"
-          />
         </div>
       )}
-    </div>
+    </>
   );
 }
